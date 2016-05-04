b1c7eae58b01e02d9ad228ed5752fb6659b66270
Arthur Gorr <art.gorr@gmail.com>
2016-05-03 19:38:13 -0500

    Finished Multi-screen tutorial

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -5,12 +5,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
 
 namespace Phoneword
 {
     [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
-    {        
+    {
+        static readonly List<string> phoneNumbers = new List<string>();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -45,12 +48,23 @@ namespace Phoneword
                 }
             };
 
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            callHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(CallHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
             callButton.Click += (object sender, EventArgs e) =>
             {
                 // On "Call" button click, try to dial phone number.
                 var callDialog = new AlertDialog.Builder(this);
                 callDialog.SetMessage("Call " + translatedNumber + "?");
                 callDialog.SetNeutralButton("Call", delegate {
+                    // add dialed number to list of called numbers.
+                    phoneNumbers.Add(translatedNumber);
+                    // enable the Call History button
+                    callHistoryButton.Enabled = true;
                     // Create intent to dial phone
                     var callIntent = new Intent(Intent.ActionCall);
                     callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
@@ -61,6 +75,7 @@ namespace Phoneword
                 // Show the alert dialog to the user and wait for response.
                 callDialog.Show();
             };
+
         }
     }
 }
